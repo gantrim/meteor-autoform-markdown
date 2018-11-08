e73d7ac3756a60ecc20fd4ca21a62b29dbffebc8
Greg Antrim <g.antrim@kunzleigh.com>
2018-11-08 13:00:10 -0500

    change package author until PR is merged

--- package.js
+++ package.js
@@ -1,25 +1,25 @@
 Package.describe({
-  name: 'q42:autoform-markdown',
-  version: '1.0.1',
-  summary: 'A simple autoform markdown with preview using perak:markdown',
-  git: 'https://github.com/Q42/meteor-autoform-markdown',
-  documentation: 'README.md'
+    name: 'gantrim:autoform-markdown',
+    version: '1.0.1',
+    summary: 'A simple autoform markdown with preview using perak:markdown',
+    git: 'https://github.com/Q42/meteor-autoform-markdown',
+    documentation: 'README.md'
 });
 
-Package.onUse(function(api) {
-  api.versionsFrom(['METEOR@0.9.3', 'METEOR@0.9.4', 'METEOR@1.0']);
-  api.use(['templating', 'reactive-var'], 'client');
-  api.use(['perak:markdown@1.0.5', 'aldeed:autoform@6.3.0'], 'client');
-  api.addFiles([
-    'markdown.html',
-    'markdown.css',
-    'jquery.selection.js',
-    'markdown.js'
-  ], 'client');
+Package.onUse(function (api) {
+    api.versionsFrom(['METEOR@0.9.3', 'METEOR@0.9.4', 'METEOR@1.0']);
+    api.use(['templating', 'reactive-var'], 'client');
+    api.use(['perak:markdown@1.0.5', 'aldeed:autoform@6.3.0'], 'client');
+    api.addFiles([
+        'markdown.html',
+        'markdown.css',
+        'jquery.selection.js',
+        'markdown.js'
+    ], 'client');
 
 });
 
-Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('q42:autoform-markdown');
+Package.onTest(function (api) {
+    api.use('tinytest');
+    api.use('gantrim:autoform-markdown');
 });
